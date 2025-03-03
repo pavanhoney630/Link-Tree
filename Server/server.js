2a22caee2190c80bbf0a4bfa1938d75717b2847a
pavankalyan <114129248+pavanhoney630@users.noreply.github.com>
2025-03-03 15:30:05 +0530

    commit3

--- Server/server.js
+++ Server/server.js
@@ -37,8 +37,11 @@ const mongo_url = process.env.MONGO_URI;
 
 mongoose
   .connect(mongo_url)
+  
   .then(() => {
+    
     console.log("MongoDB Connected...");
+    
   })
   .catch((err) => {
     console.log("MongoDB Connection Error: ", err);
